8c28d4a1a59ada999e45997647212a7808cd89fe
xuanwenj <wenjingxuan2@gmail.com>
2024-09-03 18:44:00 +1200

    Lower and upper case

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace StringDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            StringConversion();
+        }
+
+        private static void StringConversion()
+        {
+            string testString = "tHis is tHe DDD Calling!";
+            TextInfo currentTextInfo = CultureInfo.CurrentCulture.TextInfo;
+            TextInfo englishTextInfo = new CultureInfo("en-US", false).TextInfo;
+            string result;
+
+            result = testString.ToLower();
+            Console.WriteLine(result);
+
+            result = testString.ToUpper();
+            Console.WriteLine(result);
+
+            result = englishTextInfo.ToTitleCase(testString);
+            Console.WriteLine(result);
+        }
+    }
+}
