c416a1662e1e2f733fd8ba768f8399d913da3d64
xuanwenj <wenjingxuan2@gmail.com>
2024-09-03 21:24:04 +1200

    String array; escape string

--- Program.cs
+++ Program.cs
@@ -8,12 +8,16 @@ namespace StringDemo
         static void Main(string[] args)
         {
             Console.WriteLine();
-            StringConversion();
+            //StringConversion();
+            //StringAsArray();
+            EscapeString();
+            // the  main method is static, so everything it calls has to be static or instantiated 
+
         }
 
         private static void StringConversion()
         {
-            string testString = "tHis is tHe DDD Calling!";
+            string testString = "tHis is tHe DDI Calling!";
             TextInfo currentTextInfo = CultureInfo.CurrentCulture.TextInfo;
             TextInfo englishTextInfo = new CultureInfo("en-US", false).TextInfo;
             string result;
@@ -24,8 +28,34 @@ namespace StringDemo
             result = testString.ToUpper();
             Console.WriteLine(result);
 
+
             result = englishTextInfo.ToTitleCase(testString);
             Console.WriteLine(result);
+        }
+        private static void StringAsArray()
+        {
+            string testString = "Temi";
+            for (int i = 0; i < testString.Length; i++)
+            {
+                Console.WriteLine(testString[i]);
+
+            }
+        }
+        //skip strings
+        private static void EscapeString()
+        {
+            string results;
+
+            results = "This is my \"test\" solution.";
+            Console.WriteLine(results);
+
+            results = "C:\\Demo\\Test.txt";
+            Console.WriteLine(results);
+            // @ is call string literal, with this, you can directly use \ in strings,
+            // the drawback is it can't include ""
+
+            results = @"C:\\Demo\\Test.txt";
+            Console.WriteLine(results);
         }
     }
 }
